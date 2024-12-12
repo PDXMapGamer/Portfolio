877889f2d1b1aa7e99202cf8b0c862ddb9e232e7
Thomas Harvey <thomasharvey1415@gmail.com>
2024-12-12 23:58:57 +0000

    quick addition of second project and todo of what to do next

--- src/app/current-projects/page.tsx
+++ src/app/current-projects/page.tsx
@@ -1,3 +1,9 @@
 export default function Current_Project() {
-  return <p>I am currently working on this portfolio.</p>;
+  // todo Expand on this page, write more details on each of my projects and link to the github repos
+  return (
+    <p>
+      I am currently working on this portfolio and making a website to learn Japanese, playing around with an API to
+      easily compile a list of over 2000 commonly used kanjis.
+    </p>
+  );
 }
