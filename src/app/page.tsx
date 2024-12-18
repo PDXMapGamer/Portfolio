e7f2e13c36d6cb2220f4b128fb0a4c729a97acf4
Thomas Harvey <thomasharvey1415@gmail.com>
2024-12-18 23:55:10 +0000

    Changed drop down component to accept props of what the contents of the component will be

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,11 @@ import DropDown from "./components/DropDown";
 import TechStack from "./components/TechStack";
 
 export default function Home() {
+  const aboutMe =
+    "I am an aspiring programmer who wants to take thier first steps into the tech industry and start a fufilling career and finance my hobbies and future career goals.";
   return (
     <>
-      <DropDown />
+      <DropDown content={aboutMe} />
       <TechStack />
     </>
   );
