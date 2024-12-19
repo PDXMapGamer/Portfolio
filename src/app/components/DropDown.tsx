9b01a5df58442ccabee62f9fcde9fba84a11d412
Thomas Harvey <thomasharvey1415@gmail.com>
2024-12-19 00:24:52 +0000

    Attempt to fix bug that causes vercel deployment to fail

--- src/app/components/DropDown.tsx
+++ src/app/components/DropDown.tsx
@@ -1,13 +1,11 @@
-export default function DropDown() {
+export default async function DropDown(props: { content: string }) {
+  const content = (await props).content;
   return (
     <div className="collapse collapse-arrow border-base-300 bg-base-200 border mb-4">
       <input type="checkbox" />
       <div className="collapse-title text-xl font-medium">About Me.</div>
       <div className="collapse-content">
-        <p>
-          I am an aspiring programmer who wants to take thier first steps into the tech industry and start a fufilling
-          career and finance my hobbies and future career goals.
-        </p>
+        <p>{content}</p>
       </div>
     </div>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import DropDown from "./components/DropDown";
 import TechStack from "./components/TechStack";
 
 export default function Home() {
-  const aboutMe =
+  const aboutMe: string =
     "I am an aspiring programmer who wants to take thier first steps into the tech industry and start a fufilling career and finance my hobbies and future career goals.";
   return (
     <>
