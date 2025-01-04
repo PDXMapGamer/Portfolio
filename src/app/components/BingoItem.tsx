10e3ef85ae41c0ff296a47090974104daa1ce12b
Thomas Harvey <thomasharvey1415@gmail.com>
2025-01-04 14:43:40 +0000

    Displayed my 2025 bingo card on the page, made a basic setup could improve on it later

--- src/app/components/BingoItem.tsx
+++ src/app/components/BingoItem.tsx
@@ -1,9 +1,15 @@
 export default function BingoItem(props: { content: string; isFulfilled: boolean; notes: string }) {
   const { content, isFulfilled, notes } = props;
+  const stylings = "relative min-h-24"; //Ensures consistant stylings for cleared and uncleared goals
   return (
-    <div className={isFulfilled ? "bg-green-500" : "bg-gray-400"}>
-      <p>{content}</p>
-      <p>{notes}</p>
+    <div className={isFulfilled ? `bg-green-500 ${stylings}` : `bg-gray-800 ${stylings}`}>
+      <div>
+        <p className="absolute flex justify-center items-center text-center w-full h-3/4">{content}</p>
+      </div>
+      {/* Divs used to make structuring easier.*/}
+      <div>
+        <p className="absolute bottom-0 w-full h-1/4 text-center">{notes}</p>
+      </div>
     </div>
   );
 }
